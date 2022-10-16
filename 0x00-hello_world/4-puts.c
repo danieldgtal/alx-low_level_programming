25d3716ff1bc484fe4a5b9f65dc80734d2faabd8
danieldgtal <danielchiatuiro@gmail.com>
2022-10-16 05:25:59 -0700

    C program that prints exactly an output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main -Entry
+ * Description: main - function main begins the execution of the program 
  *
- * Return Always 0 (success)
+ * Return: 0 indicates that the program is sucessful
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
