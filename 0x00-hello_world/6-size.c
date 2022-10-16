a41ead6d7b1be6d856766db0cf4a75adb6904ce4
danieldgtal <danielchiatuiro@gmail.com>
2022-10-16 06:41:45 -0700

    printsize of data types on machine compiled

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return - 0 if successful
+ * Description: main - prints the size of data type of compile machine
+ * Return: 0 indicates that the programme is succesful
  */
 int main(void)
 {
@@ -11,10 +11,11 @@ int main(void)
 	char d;
 	float f;
 
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %lu byts(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a char: %lu bytes(s)\n",(unsigned long)sizeof(d));
-	printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
 	return (0);
 }
