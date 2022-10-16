43631f3f55877318865c1ee7bc3c203f0e4c4f67
danieldgtal <danielchiatuiro@gmail.com>
2022-10-16 05:35:17 -0700

    C program that prints exact keyword using 'put' keyword

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 /**
- * Description: main - function main begins the execution of the program 
- *
+ * Description: main - function main begins the execution of the program
  * Return: 0 indicates that the program is sucessful
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
